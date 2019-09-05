70dc2457053b1b52d3b00479171fe83f7c6bbec3
Andy Jeong <unjin89@gmail.com>
2019-09-05 14:22:36 -0400

    completed exercise 1

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+#print("Hello Again")
+#print("I like typing this.")
+#print("This is so fun.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+#print("Testing 1, 2, 3...")
