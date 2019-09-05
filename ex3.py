00dc928bb0be850140f79115130166405d8b8f02
Andy Jeong <unjin89@gmail.com>
2019-09-05 14:41:16 -0400

    completed exercise 3

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:")
+# Divides 30 with 6 and adds it to 25
+print("Hens", 25.0 + 30.0 / 6.0)
+# Multiply 25 by 3, modulous the product by 4. Remainder is 3 and subtract that from 100
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("Now I will count the eggs:")
+# 4 % 2 = 0, 1/4 = 0.25
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print("How about some more")
+
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
