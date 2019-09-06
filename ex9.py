8d7c75eb24acd3337d3f9ac46666393a2611ce4e
Andy Jeong <unjin89@gmail.com>
2019-09-06 14:58:42 -0400

    completed exercise 9

---
+++ ex9.py
@@ -0,0 +1,14 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# \n prints on new line
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# Three double-quotes prints lines indented
+print("""
+    There's something going on here.
+    WIth the three double-quotes.
+    We'll be able to type as much as we like.
+    Even 4 lines if we ant, or 5, or 6.
+""")
