ac8d9f502d8b7851aff1411b76f819be4cd1a180
Andy Jeong <unjin89@gmail.com>
2019-09-06 15:20:27 -0400

    completed exercise 11

---
+++ ex11.py
@@ -0,0 +1,15 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print("What is your favorite food?", end=' ')
+food = input()
+print("Who is your favorite artist?", end=' ')
+artist = input()
+
+print(f"So you like to eat {food} and your favorite artist is {artist}")
