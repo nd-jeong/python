5073466960eefb70e78343c8ba5ac7063889951c
Andy Jeong <unjin89@gmail.com>
2019-09-10 12:48:08 -0400

    completed exercise 22

---
+++ ex22.py
@@ -0,0 +1,21 @@
+# print - Displays string and/or variables input inside parentheses
+
+# f / format - replaces any variables with curly bracers with value that is set or input by user
+
+# \n - creates new line
+
+# input() - asks for user input; Anything inside parenthesis will be a prompt for the user
+
+# open() - opens the file
+
+# close() - closes the open file
+
+# filename.read() - Reads the file
+
+# filename.write() - Writes anything inside parenthesis to the specific file
+
+# def - defines function
+
+# filename.seek(#) - Will find the specific # line in the specified file
+
+# return - returns the end value
